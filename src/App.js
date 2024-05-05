903e4999814c8418378911723646175b4235a1fd
Axel Motulsky <a.motulsky@wustl.edu>
2024-05-04 21:05:42 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import CatGallery from './components/CatGallery';
+import BreedList from './components/BreedList';
+import FavoritesDropdown from './components/FavoritesDropdown';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [breedId, setBreedId] = useState('');
+    const [favorites, setFavorites] = useState({});
+    const featuredCat = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg'; // Fixed image
+
+    function toggleFavorite(catId, catUrl) {
+        setFavorites(prev => ({
+            ...prev,
+            [catId]: !prev[catId] ? catUrl : null
+        }));
+    }
+
+    return (
+        <div className="App">
+            <div className="sidebar">
+                <FavoritesDropdown favorites={favorites} />
+            </div>
+            <div className="App-content">
+                <Navbar className="show-favorites" />
+                <header className="App-header">
+                    <h1>Welcome to the Purrfect, a Cat Shelter dedicated for finding every cat a loving home.</h1>
+                    <img src={featuredCat} alt="Featured Cat" className="featured-cat"/>
+                    <p>Wouldn't you want to adopt this cat? Discover your forever companion from our available cats today!</p>
+                </header>
+                <BreedList onBreedSelect={setBreedId} />
+                <CatGallery breedId={breedId} toggleFavorite={toggleFavorite} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
